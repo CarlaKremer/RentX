db304cb22ad69fc5bc3834939e34b97b68adf9dc
CarlaKremer <carlakremerf@gmail.com>
2022-05-16 16:02:47 -0300

    feat: header in home and logo dependencies

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,27 @@
+import { StatusBar } from 'react-native';
 import React from 'react'
+import { RFValue } from 'react-native-responsive-fontsize'
 
+import Logo from '../../assets/logo.svg'
 import {
   Container, 
-  Title
+  Header
 } from './styles';
 
 export function Home() {
 return (
   <Container>
-    <Title>Home</Title>
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />
+    <Header>
+      <Logo 
+        width={RFValue(108)}
+        height={RFValue(12)}
+      />
+    </Header>
   </Container>
 );
 }
